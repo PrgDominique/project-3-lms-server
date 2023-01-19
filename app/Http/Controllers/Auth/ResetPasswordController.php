8ae45462d5ec5a4e9854ca534803bf0287d0f5b6
jasonlerit <jasonlerit077@gmail.com>
2023-01-19 19:38:47 +0800

    added validation in reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,18 +3,43 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Utils\ValidationUtil;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ResetPasswordController extends Controller
 {
     public function update(Request $request)
     {
-        // Get id, token, password
         $id = $request->id;
         $token = $request->token;
         $password = $request->password;
 
-        // TODO: Validate inputs
+        $result = ValidationUtil::validateId($id);
+        if ($result != null) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $result,
+            ], 400);
+        }
+
+        $result = ValidationUtil::validateToken($token);
+        if ($result != null) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $result,
+            ], 400);
+        }
+
+        $result = ValidationUtil::validatePassword($password);
+        if ($result != null) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $result,
+            ], 400);
+        }
+
         // TODO: Find user by id
         // TODO: Check token
         // TODO: Update password
